d4fb933751898c6ac9172e4be5d604ab63d6b0c9
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-12 08:36:26 -0400

    test tests testset set

--- src/MonthlyCreditCardBalance.java
+++ src/MonthlyCreditCardBalance.java
@@ -1,5 +1,11 @@
-public class Main {
+public class MonthlyCreditCardBalance {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double creditCardBalance = 5000;
+        double INTEREST_RATE = 1.17;
+
+        creditCardBalance *= INTEREST_RATE;
+        System.out.println("The credit card balance after a month is $" + creditCardBalance);
+        creditCardBalance *= INTEREST_RATE;
+        System.out.println("The credit card balance after two months is $" + creditCardBalance);
     }
 }
